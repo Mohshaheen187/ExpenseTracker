e03f3546e4ab728ad286a5d547f8fc252c6682a6
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2023-09-13 00:44:08 +0200

    Added new views

--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -14,7 +14,7 @@ struct ExpenseTrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            MainView()
+            TabViews()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
---
+++ ExpenseTracker/View/ChartsView.swift
@@ -0,0 +1,25 @@
+//
+//  ChartsView.swift
+//  ExpenseTracker
+//
+//  Created by Mohammed Shaheen on 13.09.23.
+//
+
+import SwiftUI
+
+struct ChartsView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Coming Soon!")
+            }
+            .navigationTitle("Charts")
+        }
+    }
+}
+
+struct ChartsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartsView()
+    }
+}
