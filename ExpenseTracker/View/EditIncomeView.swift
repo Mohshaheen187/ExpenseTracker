be0f024c6c97bc121d14608642ca9bc0ead47abd
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2023-09-13 12:59:59 +0200

    Modified the Edit view

--- ExpenseTracker/View/EditIncomeView.swift
+++ ExpenseTracker/View/EditIncomeView.swift
@@ -22,31 +22,59 @@ struct EditIncomeView: View {
     //MARK: BODY
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
-                    TextField("\(income.title!)", text: $title)
-                        .onAppear {
-                            title = income.title!
-                            amount = income.amount
-                            date = income.date!
+            VStack(spacing: 25) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Title")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    TextField("\(income.title!)", text: $title, axis: .vertical)
+                        .padding(12)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, style: StrokeStyle(lineWidth: 1))
+                                .opacity(0.5)
                         }
-                    VStack {
-                        Text("Amount: \(String(format: "%.2f", income.amount))")
-                        TextField("Amount", value: $amount, formatter: NumberFormatter())
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    DatePicker("Payment Day", selection: $date, displayedComponents: [.date])
                 }
+                .padding([.leading, .trailing])
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Amount")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    TextField("\(income.amount)", value: $amount, formatter: NumberFormatter())
+                        .keyboardType(.decimalPad)
+                        .padding(12)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, style: StrokeStyle(lineWidth: 1))
+                                .opacity(0.5)
+                        }
+                }
+                .padding([.leading, .trailing])
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Date")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    DatePicker("When did you get the money?", selection: $date, in: ...Date(), displayedComponents: [.date])
+                }
+                .padding([.leading, .trailing])
+                
+                Spacer()
             }
             .navigationTitle("Edit Income")
             .toolbar {
-                ToolbarItem(placement: .bottomBar) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Done") {
                         DataController().editIncomeTransaction(incomeTrans: income, title: title, amount: amount, date: date, context:  moc)
                         dismiss()
                     }
                 }
+            }
+            .task {
+                title = income.title!
+                amount = income.amount
+                date = income.date!
             }
         }
     }
--- ExpenseTracker/View/EditOutcomeView.swift
+++ ExpenseTracker/View/EditOutcomeView.swift
@@ -22,31 +22,59 @@ struct EditOutcomeView: View {
     //MARK: BODY
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
-                    TextField("\(outcome.title!)", text: $title)
-                        .onAppear {
-                            title = outcome.title!
-                            amount = outcome.amount
-                            date = outcome.date!
+            VStack(spacing: 25) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Title")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    TextField("\(outcome.title!)", text: $title, axis: .vertical)
+                        .padding(12)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, style: StrokeStyle(lineWidth: 1))
+                                .opacity(0.5)
                         }
-                    VStack {
-                        Text("Amount: \(String(format: "%.2f", outcome.amount))")
-                        TextField("Amount", value: $amount, formatter: NumberFormatter())
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    DatePicker("Payment Day", selection: $date, displayedComponents: [.date])
                 }
+                .padding([.leading, .trailing])
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Amount")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    TextField("\(outcome.amount)", value: $amount, formatter: NumberFormatter())
+                        .keyboardType(.decimalPad)
+                        .padding(12)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, style: StrokeStyle(lineWidth: 1))
+                                .opacity(0.5)
+                        }
+                }
+                .padding([.leading, .trailing])
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Date")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    DatePicker("When did you get the money?", selection: $date, in: ...Date(), displayedComponents: [.date])
+                }
+                .padding([.leading, .trailing])
+                
+                Spacer()
             }
             .navigationTitle("Edit Income")
             .toolbar {
-                ToolbarItem(placement: .bottomBar) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Done") {
                         DataController().editOutcomeTransaction(outcomeTrans: outcome, title: title, amount: amount, date: date, context: moc)
                         dismiss()
                     }
                 }
+            }
+            .task {
+                title = outcome.title!
+                amount = outcome.amount
+                date = outcome.date!
             }
         }
     }
