9477ac24c6184f4666762576b02303d05b3bd156
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2023-09-19 22:55:59 +0200

    arranged the lines

--- ExpenseTracker/ViewModel/IncomeChartsViewModel.swift
+++ ExpenseTracker/ViewModel/IncomeChartsViewModel.swift
@@ -11,17 +11,17 @@ import Combine
 
 class IncomeChartViewModel: ObservableObject {
     @Published var incomeData: [(Category, Double)] = []
-
+    
     init(incomes: [Income]) {
         // Group incomes by category and calculate total income for each category
         let groupedIncomes = Dictionary(grouping: incomes, by: { $0.category })
-
+        
         // Map the grouped data to the desired format (Category, Double)
         incomeData = groupedIncomes.compactMap { (categoryString, incomes) in
             guard let category = Category(rawValue: categoryString!) else {
                 return nil // Skip invalid categories
             }
-
+            
             let totalIncome = incomes.map { $0.amount }.reduce(0, +)
             return (category, totalIncome)
         }
